eae11759746c2637c73bdd457f66e2702d054e74
yoonchaiyoung <domo62@naver.com>
2020-09-25 09:25:38 +0900

    ObjectId

--- router/web.js
+++ router/web.js
@@ -1,6 +1,7 @@
 // 라우터
 const express = require("express");
 const router = express.Router();
+const { ObjectId } = require("mongodb");
 
 // 라우터 모듈 내보내기
 module.exports = (app) => {
@@ -50,5 +51,19 @@ module.exports = (app) => {
           })
     })
     
+    router.get("/friends/show/:id", (req, resp) => {
+        console.log("id:", req.params.id);
+        //  _id 필드는 문자열이 아니라 ObjectId라는 특수한 객체이다
+        let db = app.get("db");
+        db.collection("friends").findOne(
+            { _id: ObjectId(req.params.id)}
+        ).then(result => {
+            console.log(result);
+            //  TODO: 해당 질의에 매칭되는 레코드가 없을 때의 처리
+            resp.status(200).render("friend_show", { friend: result });
+        }).catch(err => {
+            console.error(err);
+        })
+    });
     return router;
 }
